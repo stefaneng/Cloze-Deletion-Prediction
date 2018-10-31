930072a8d9f6bc6568c3ec013362649422935db7
Stefan Eng <stefaneng13@gmail.com>
2018-10-31 20:15:02 +0100

    Compare BLSTM and LSTM Models

--- atta_train.py
+++ atta_train.py
@@ -7,7 +7,7 @@ import itertools
 import numpy as np
 
 from keras.models import Model
-from keras.layers import Dense, LSTM, Embedding, Input, Dropout, concatenate
+from keras.layers import Dense, LSTM, Embedding, Input, Dropout, concatenate, Bidirectional
 
 def create_training_example(sentence, pos_tags, sent_id, tokenizer, cloze_pos=['NN', 'VB', 'JJ'], window_size=3):
     for i, word in enumerate(sentence):   
@@ -66,34 +66,68 @@ input_length = before_length + after_length
 cats_length = y_cat.shape[1]
 
 ## Model 1
-before_input = Input(shape=(before_length,), name="before_input")
-after_input = Input(shape=(after_length,), name="after_input")
+def simple_lstm():
+    before_input = Input(shape=(before_length,), name="before_input")
+    after_input = Input(shape=(after_length,), name="after_input")
 
-inputs = concatenate([before_input, after_input])
+    inputs = concatenate([before_input, after_input])
 
-embed = Embedding(vocab_size, embed_size, input_length=input_length)(inputs)
-lstm = LSTM(50, dropout=0.1)(embed)
+    embed = Embedding(vocab_size, embed_size, input_length=input_length)(inputs)
+    lstm = LSTM(50, dropout=0.1)(embed)
 
-# Word prediction softmax
-word_pred = Dense(vocab_size, activation='softmax', name='word_prediction')(lstm)
+    # Word prediction softmax
+    word_pred = Dense(vocab_size, activation='softmax', name='word_prediction')(lstm)
 
-# This creates a model that includes
-# the Input layer and two Dense layers outputs
-model = Model(inputs=[before_input, after_input], outputs=word_pred)
+    # This creates a model that includes
+    # the Input layer and two Dense layers outputs
+    model = Model(inputs=[before_input, after_input], outputs=word_pred)
 
-csv_logger = CSVLogger('./atta_lstm_log.csv', append=True, separator=',')
-earlystopping = EarlyStopping(monitor='loss', patience=2)
+    csv_logger = CSVLogger('./results/atta_lstm_log.csv', append=True, separator=',')
+    earlystopping = EarlyStopping(monitor='loss', patience=2)
 
-model.compile(optimizer='adam',
-                loss={
-                    'word_prediction': 'categorical_crossentropy'
-                },
-                metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                    loss={
+                        'word_prediction': 'categorical_crossentropy'
+                    },
+                    metrics=['accuracy'])
 
-model.summary()
+    model.summary()
 
-history = model.fit([X_before, X_after], y_cat, batch_size=32, epochs=40,
-                callbacks=[csv_logger, earlystopping])
-model.save('./atta_lstm.hdf5')
-print(history.history)
+    history = model.fit([X_before, X_after], y_cat, batch_size=32, epochs=40,
+                    callbacks=[csv_logger, earlystopping])
+    model.save('./models/atta_lstm.hdf5')
+    return model
 
+def simple_blstm():
+    before_input = Input(shape=(before_length,), name="before_input")
+    after_input = Input(shape=(after_length,), name="after_input")
+
+    inputs = concatenate([before_input, after_input])
+
+    embed = Embedding(vocab_size, embed_size, input_length=input_length)(inputs)
+    blstm = Bidirectional(LSTM(50, dropout=0.1))(embed)
+
+    # Word prediction softmax
+    word_pred = Dense(vocab_size, activation='softmax', name='word_prediction')(blstm)
+
+    # This creates a model that includes
+    # the Input layer and two Dense layers outputs
+    model = Model(inputs=[before_input, after_input], outputs=word_pred)
+
+    csv_logger = CSVLogger('./results/atta_blstm_log.csv', append=True, separator=',')
+    earlystopping = EarlyStopping(monitor='loss', patience=2)
+
+    model.compile(optimizer='adam',
+                    loss={
+                        'word_prediction': 'categorical_crossentropy'
+                    },
+                    metrics=['accuracy'])
+
+    model.summary()
+
+    history = model.fit([X_before, X_after], y_cat, batch_size=32, epochs=40,
+                    callbacks=[csv_logger, earlystopping])
+    model.save('./models/atta_blstm.hdf5')
+    return model
+
+simple_blstm()
